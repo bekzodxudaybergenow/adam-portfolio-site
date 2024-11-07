957d889807c8038e86ed212dc6b8cc4c0bea8e3b
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-11-07 22:38:12 +0500

    Update the javascript part of the header section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 let sidebarOpenBtn = document.querySelector('.sidebar-humburger--btn');
 let sidebar = document.querySelector('.sidebar');
+let closeBtn = document.querySelector('.close-btn');
+let headerShade = document.querySelector('.header-shade');
 
 
 
 sidebarOpenBtn.addEventListener('click', () => {
     sidebar.classList.add('sidebar-show');
+    headerShade.classList.add('shade');
+
+})
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-show');
+    headerShade.classList.remove('shade');
+})
+
+headerShade.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-show');
+    headerShade.classList.remove('shade');
 })
