28376c6f5fc6b19277bcfd9efc535ed72b435ada
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-11-11 23:44:09 +0500

    Update Header section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let sidebarOpenBtn = document.querySelector('.sidebar-humburger--btn');
 let sidebar = document.querySelector('.sidebar');
 let closeBtn = document.querySelector('.close-btn');
 let headerShade = document.querySelector('.header-shade');
+let headerNav = document.querySelector('.header-nav');
 
 
 
@@ -19,3 +20,14 @@ headerShade.addEventListener('click', () => {
     sidebar.classList.remove('sidebar-show');
     headerShade.classList.remove('shade');
 })
+
+// window.onscroll = () => {
+//     let temp = 0;
+//     if(window.scrollY > 0) {
+//         // headerNav.style = 'transform: translateY(-100%)';
+//         console.log(window.scrollY);
+//         temp = window.scrollY;
+//     }
+//     // if(temp <)
+//     console.log(`Temp: ${temp}`);
+// }
